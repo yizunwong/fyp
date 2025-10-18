44d7df0ecb5c6e9b6df9edb2e039358543553eef
yizunwong <yizunwong@gmail.com>
2025-10-19 03:02:24 +0800

    done file adjust

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './api/user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './api/auth/auth.module';
 import { FarmerModule } from './api/farmer/farmer.module';
 import { FarmModule } from './api/farm/farm.module';
 import { ProduceModule } from './api/produce/produce.module';
